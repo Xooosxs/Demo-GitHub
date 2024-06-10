f68271c5452ed27f9255edcd11b181cdc25567ea
Xooosxs <xooosxs97@gmail.com>
2024-06-10 10:09:15 +0700

    Update LoginSystem.java

--- LoginSystem.java
+++ LoginSystem.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class LoginSystem {
-    
+
+    public static int main(String[] args){
+        Sysytem.out.println("Test");
+    }
 
     
 }
